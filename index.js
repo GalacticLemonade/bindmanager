ca94c3f33fe202aa2e25e4d8cef46a1941f06fa9
GalacticLemonade <aidenmoore1820@gmail.com>
2024-01-06 18:10:41 -0700

    s

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const fs = require('node:fs')
 
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 
+//plan on just supporting A, TXT and CNAME records for now
 
-app.listen(3000)
+app.get("/", (req, res, next) => {
+    if (!req.cookies['signedin']) {
+        res.redirect('/login/')
+        return
+    }
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+app.post('/api/login/', (req, res) => {
+    let readPass = fs.readFileSync(__dirname + '/pass.txt', 'utf-8')
+    if (req.body.pass == readPass) {
+        res.cookie('signedin', true)
+        res.redirect('/')
+        return
+    }
+
+    res.redirect('/login/')
+})
+
+app.get('/login/', (req, res) => {
+    res.sendFile(__dirname + "/login.html")
+})
+
+app.listen(3050)
